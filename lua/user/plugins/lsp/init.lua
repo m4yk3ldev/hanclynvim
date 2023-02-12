d4f8a4287b324108e857cfd33fe6d6542b7d5ab9
Maykel Arias Torres <hanibal920915@gmail.com>
2023-02-12 11:32:20 -0300

    #Fix 2 Rename plugins

--- lua/user/plugins/lsp/init.lua
+++ lua/user/plugins/lsp/init.lua
@@ -1,6 +1,6 @@
-require("user.plugins.lsp.lspkind") -- Cargando LSP
-require("user.plugins.lsp.lsp_color") -- Cargando LSP Color
+require("user.plugins.lsp.lspcolor.rc") -- Cargando LSP Color
 require("user.plugins.lsp.mason") -- Cargando Mason
 require("user.plugins.lsp.lsp_config") -- Cargando Mason
 require("user.plugins.lsp.lspsaga") -- Cargando LSP Saga
 require("user.plugins.lsp.null_ls") -- Cargando null
+require("user.plugins.lsp.lspkind.rc") -- Cargando LSPKind Icons
--- lua/user/plugins/lsp/lspkind.rc.lua
+++ lua/user/plugins/lsp/lspkind.rc.lua
@@ -8,14 +8,12 @@ lspkind.init({
 	--
 	-- default: true
 	mode = "symbol",
-
 	-- default symbol map
 	-- can be either 'default' (requires nerd-fonts font) or
 	-- 'codicons' for codicon preset (requires vscode-codicons font)
 	--
 	-- default: 'default'
 	preset = "codicons",
-
 	-- override preset symbols
 	--
 	-- default: {}
