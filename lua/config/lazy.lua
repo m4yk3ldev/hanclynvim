e4b8db6364269340597a988bbcc507df5632aedb
maykel <hanibal920915@gmail.com>
2025-02-23 20:18:45 -0300

    Add lazy.nvim setup configuration

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.json" },
