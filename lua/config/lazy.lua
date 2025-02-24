e2e874570011bbac7e1c16b5aa50c4b4191d6fe7
maykel <hanibal920915@gmail.com>
2025-02-23 21:17:32 -0300

    🔧 Added essential language plugins
    - Imported Docker, Markdown, and TOML language plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,6 +34,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.biome" },
     -- import/override with your plugins
