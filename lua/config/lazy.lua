3aeb2ba48751e1cbbcde1db92da38b13e37f0c46
Maykel Arias Torres <hanibal920915@gmail.com>
2025-02-26 11:14:29 -0300

    adding commit

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -40,33 +40,23 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.biome" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+    version = false,
   },
   install = { colorscheme = { "gruvbox" } },
   checker = {
-    enabled = true, -- check for plugin updates periodically
-  }, -- automatically check for plugin updates
+    enabled = true,
+  },
   performance = {
     cache = {
-      enabled = true, -- cache loaded plugins
+      enabled = true,
     },
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
